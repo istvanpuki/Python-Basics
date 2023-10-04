4c1f420090631ae1445a3ff5c572b78d803e454a
istvanpuki <istvan.puki92@gmail.com>
2023-10-04 15:58:19 +0200

    Osztályok magyarázva

--- 8. foglalkozás/MIKECZ_08_osztályok.py
+++ 8. foglalkozás/MIKECZ_08_osztályok.py
@@ -12,3 +12,8 @@ Ház1 =Ingatlan("Nyh", 66, 10000000)
 print(Ház1)
 print(Ház1.cím)
 print(Ház1.négyzetméterÁr())
+
+Ház2 =Ingatlan("Deb", 72, 20000000)
+print(Ház2)
+print(Ház2.cím)
+print(Ház2.négyzetméterÁr())
