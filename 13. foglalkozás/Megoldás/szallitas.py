f5c6408fd56f3409248af83d1332bb61ddf38034
istvanpuki <istvan.puki92@gmail.com>
2023-11-03 11:44:15 +0100

    Érettségi_python_23_október_közép

---
+++ 13. foglalkozás/Megoldás/szallitas.py
@@ -0,0 +1,31 @@
+#1. feladat Eltároljuk a 15 számot egy megfelelő adatszerkezetben.
+itemsWeights = [16, 8, 9, 4, 3, 2, 4, 7, 7, 12, 3, 5, 4, 3, 2]
+#print (itemsWeights)
+
+#2. feladat
+sumWeights = 0
+
+for item in itemsWeights:
+    #print(item)
+    sumWeights = sumWeights + item
+
+print(f"2. feladat \nA tárgyak tömegének összege: {sumWeights} kg\n ")
+
+#3. feladat
+boxes = []
+count = 0
+
+for item in itemsWeights:
+    if count + item < 21:
+        count = count + item
+    else:
+        boxes.append(count)
+        count = 0
+        count = count + item
+
+boxes.append(count)
+
+print("3. feladat")
+print(f"A dobozok tartalmának tömege (kg): {boxes}")
+print(f"A szükséges dobozok száma: {len(boxes)}")
+  
