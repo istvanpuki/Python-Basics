495b3f91b845a94becf75cac11654ae164e8b5b5
istvanpuki <istvan.puki92@gmail.com>
2023-11-16 15:14:34 +0100

    Ez most már tényleg hibátlan! :)

--- 12. foglalkozás/date_solution_clear.py
+++ 12. foglalkozás/date_solution_clear.py
@@ -5,13 +5,16 @@ def date(y, m, d):
     dateFromConsole = datetime(year=y,month=m,day=d)
     
     days = dateFromConsole.strftime("%j")
-    #print(days[0], days[1], days[2])
+    print(type(days))
+    print(days[0], days[1], days[2])
+    formatDays = ""
     
-    if days[0] == '0':
-        formatDays = days.replace(days[0],"")
-    if days[1] == '0':
-        formatDays = days.replace(days[1],"")
-    
+    for day in days:
+        if day == '0' and days[0] == '0':
+            formatDays = days.replace(day, "")
+            if days[2] == '0':
+                formatDays = formatDays + '0'
+
     return "A megadott dátum az év " + formatDays+ ". napja"
 
 showDays = date( int( input("Év: ") ), int( input("Hónap: ") ), int( input("Nap: ") ) )
